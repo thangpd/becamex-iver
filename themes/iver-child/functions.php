d6d7380083d19d9149137706ca265d5477e0d78a
thangpd <5cm.1s.takaki@gmail.com>
2019-07-01 16:43:43 +0700

    css newcity

--- themes/iver-child/functions.php
+++ themes/iver-child/functions.php
@@ -7,6 +7,7 @@ if ( !function_exists( 'select_iver_child_theme_enqueue_scripts' ) ) {
 		$parent_style = 'select-iver-default-style';
 
 		wp_enqueue_style( 'select-iver-child-style', get_stylesheet_directory_uri() . '/style.css', array( $parent_style ) );
+		wp_enqueue_style( 'select-iver-child-style-newcity', get_stylesheet_directory_uri() . '/style-newcity.css', array( $parent_style ) );
 	}
 	
 	add_action( 'wp_enqueue_scripts', 'select_iver_child_theme_enqueue_scripts' );
